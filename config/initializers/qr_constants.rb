538015c1a21d4fb91d3530be15584ebb3fbfb741
Shawn Karber <smkarber@gmail.com>
2017-03-20 10:31:11 -0500

    Set global qr code constants

---
+++ config/initializers/qr_constants.rb
@@ -0,0 +1,1063 @@
+CHARACTER_CAPACITY = {
+  1 => {
+    L: {
+      numeric: 41,
+      alphanumeric: 25,
+      byte: 17,
+      kanji: 10
+    },
+    M: {
+      numeric: 34,
+      alphanumeric: 20,
+      byte: 14,
+      kanji: 8
+    },
+    Q: {
+      numeric: 27,
+      alphanumeric: 16,
+      byte: 11,
+      kanji: 7
+    },
+    H: {
+      numeric: 17,
+      alphanumeric: 10,
+      byte: 7,
+      kanji: 4
+    },
+  },
+  2 => {
+    L: {
+      numeric: 77,
+      alphanumeric: 47,
+      byte: 32,
+      kanji: 20
+    },
+    M: {
+      numeric: 63,
+      alphanumeric: 38,
+      byte: 26,
+      kanji: 16
+    },
+    Q: {
+      numeric: 48,
+      alphanumeric: 29,
+      byte: 20,
+      kanji: 12
+    },
+    H: {
+      numeric: 34,
+      alphanumeric: 20,
+      byte: 14,
+      kanji: 8
+    },
+  },
+  3 => {
+    L: {
+      numeric: 127,
+      alphanumeric: 77,
+      byte: 53,
+      kanji: 32
+    },
+    M: {
+      numeric: 101,
+      alphanumeric: 61,
+      byte: 42,
+      kanji: 26
+    },
+    Q: {
+      numeric: 77,
+      alphanumeric: 47,
+      byte: 32,
+      kanji: 20
+    },
+    H: {
+      numeric: 58,
+      alphanumeric: 35,
+      byte: 24,
+      kanji: 15
+    },
+  },
+  4 => {
+    L: {
+      numeric: 187,
+      alphanumeric: 114,
+      byte: 78,
+      kanji: 48
+    },
+    M: {
+      numeric: 149,
+      alphanumeric: 90,
+      byte: 62,
+      kanji: 38
+    },
+    Q: {
+      numeric: 111,
+      alphanumeric: 67,
+      byte: 46,
+      kanji: 28
+    },
+    H: {
+      numeric: 82,
+      alphanumeric: 50,
+      byte: 34,
+      kanji: 21
+    },
+  },
+  5 => {
+    L: {
+      numeric: 255,
+      alphanumeric: 154,
+      byte: 106,
+      kanji: 65
+    },
+    M: {
+      numeric: 202,
+      alphanumeric: 122,
+      byte: 84,
+      kanji: 52
+    },
+    Q: {
+      numeric: 144,
+      alphanumeric: 87,
+      byte: 60,
+      kanji: 37
+    },
+    H: {
+      numeric: 106,
+      alphanumeric: 64,
+      byte: 44,
+      kanji: 27
+    },
+  },
+  6 => {
+    L: {
+      numeric: 322,
+      alphanumeric: 195,
+      byte: 134,
+      kanji: 82
+    },
+    M: {
+      numeric: 255,
+      alphanumeric: 154,
+      byte: 106,
+      kanji: 65
+    },
+    Q: {
+      numeric: 178,
+      alphanumeric: 108,
+      byte: 74,
+      kanji: 45
+    },
+    H: {
+      numeric: 139,
+      alphanumeric: 84,
+      byte: 58,
+      kanji: 36
+    }
+  },
+  7 => {
+    L: {
+      numeric: 370,
+      alphanumeric: 224,
+      byte: 154,
+      kanji: 95
+    },
+    M: {
+      numeric: 293,
+      alphanumeric: 178,
+      byte: 122,
+      kanji: 75
+    },
+    Q: {
+      numeric: 207,
+      alphanumeric: 125,
+      byte: 86,
+      kanji: 53
+    },
+    H: {
+      numeric: 154,
+      alphanumeric: 93,
+      byte: 64,
+      kanji: 39
+    }
+  },
+  8 => {
+    L: {
+      numeric: 461,
+      alphanumeric: 279,
+      byte: 192,
+      kanji: 118
+    },
+    M: {
+      numeric: 365,
+      alphanumeric: 221,
+      byte: 152,
+      kanji: 93
+    },
+    Q: {
+      numeric: 259,
+      alphanumeric: 157,
+      byte: 108,
+      kanji: 66
+    },
+    H: {
+      numeric: 202,
+      alphanumeric: 122,
+      byte: 84,
+      kanji: 52
+    }
+  },
+  9 => {
+    L: {
+      numeric: 552,
+      alphanumeric: 335,
+      byte: 230,
+      kanji: 141
+    },
+    M: {
+      numeric: 432,
+      alphanumeric: 262,
+      byte: 180,
+      kanji: 111
+    },
+    Q: {
+      numeric: 312,
+      alphanumeric: 189,
+      byte: 130,
+      kanji: 80
+    },
+    H: {
+      numeric: 235,
+      alphanumeric: 143,
+      byte: 98,
+      kanji: 60
+    }
+  },
+  10 => {
+    L: {
+      numeric: 652,
+      alphanumeric: 395,
+      byte: 271,
+      kanji: 167
+    },
+    M: {
+      numeric: 513,
+      alphanumeric: 311,
+      byte: 213,
+      kanji: 131
+    },
+    Q: {
+      numeric: 364,
+      alphanumeric: 221,
+      byte: 151,
+      kanji: 93
+    },
+    H: {
+      numeric: 288,
+      alphanumeric: 174,
+      byte: 119,
+      kanji: 74
+    }
+  },
+  11 => {
+    L: {
+      numeric: 772,
+      alphanumeric: 468,
+      byte: 321,
+      kanji: 198
+    },
+    M: {
+      numeric: 604,
+      alphanumeric: 366,
+      byte: 251,
+      kanji: 155
+    },
+    Q: {
+      numeric: 427,
+      alphanumeric: 259,
+      byte: 177,
+      kanji: 109
+    },
+    H: {
+      numeric: 331,
+      alphanumeric: 200,
+      byte: 137,
+      kanji: 85
+    }
+  },
+  12 => {
+    L: {
+      numeric: 883,
+      alphanumeric: 535,
+      byte: 367,
+      kanji: 226
+    },
+    M: {
+      numeric: 691,
+      alphanumeric: 419,
+      byte: 287,
+      kanji: 177
+    },
+    Q: {
+      numeric: 489,
+      alphanumeric: 296,
+      byte: 203,
+      kanji: 125
+    },
+    H: {
+      numeric: 374,
+      alphanumeric: 227,
+      byte: 155,
+      kanji: 96
+    }
+  },
+  13 => {
+    L: {
+      numeric: 1022,
+      alphanumeric: 619,
+      byte: 425,
+      kanji: 262
+    },
+    M: {
+      numeric: 796,
+      alphanumeric: 483,
+      byte: 331,
+      kanji: 204
+    },
+    Q: {
+      numeric: 580,
+      alphanumeric: 352,
+      byte: 241,
+      kanji: 149
+    },
+    H: {
+      numeric: 427,
+      alphanumeric: 259,
+      byte: 177,
+      kanji: 109
+    }
+  },
+  14 => {
+    L: {
+      numeric: 1101,
+      alphanumeric: 667,
+      byte: 458,
+      kanji: 282
+    },
+    M: {
+      numeric: 871,
+      alphanumeric: 528,
+      byte: 362,
+      kanji: 223
+    },
+    Q: {
+      numeric: 621,
+      alphanumeric: 376,
+      byte: 258,
+      kanji: 159
+    },
+    H: {
+      numeric: 468,
+      alphanumeric: 283,
+      byte: 194,
+      kanji: 120
+    }
+  },
+  15 => {
+    L: {
+      numeric: 1250,
+      alphanumeric: 758,
+      byte: 520,
+      kanji: 320
+    },
+    M: {
+      numeric: 991,
+      alphanumeric: 600,
+      byte: 412,
+      kanji: 254
+    },
+    Q: {
+      numeric: 703,
+      alphanumeric: 426,
+      byte: 292,
+      kanji: 180
+    },
+    H: {
+      numeric: 530,
+      alphanumeric: 321,
+      byte: 220,
+      kanji: 136
+    }
+  },
+  16 => {
+    L: {
+      numeric: 1408,
+      alphanumeric: 854,
+      byte: 586,
+      kanji: 361
+    },
+    M: {
+      numeric: 1082,
+      alphanumeric: 656,
+      byte: 450,
+      kanji: 277
+    },
+    Q: {
+      numeric: 775,
+      alphanumeric: 470,
+      byte: 322,
+      kanji: 198
+    },
+    H: {
+      numeric: 602,
+      alphanumeric: 365,
+      byte: 250,
+      kanji: 154
+    }
+  },
+  17 => {
+    L: {
+      numeric: 1548,
+      alphanumeric: 938,
+      byte: 644,
+      kanji: 397
+    },
+    M: {
+      numeric: 1212,
+      alphanumeric: 734,
+      byte: 504,
+      kanji: 310
+    },
+    Q: {
+      numeric: 876,
+      alphanumeric: 531,
+      byte: 364,
+      kanji: 224
+    },
+    H: {
+      numeric: 674,
+      alphanumeric: 408,
+      byte: 280,
+      kanji: 173
+    }
+  },
+  18 => {
+    L: {
+      numeric: 1725,
+      alphanumeric: 1046,
+      byte: 718,
+      kanji: 442
+    },
+    M: {
+      numeric: 1346,
+      alphanumeric: 816,
+      byte: 560,
+      kanji: 345
+    },
+    Q: {
+      numeric: 948,
+      alphanumeric: 574,
+      byte: 394,
+      kanji: 243
+    },
+    H: {
+      numeric: 746,
+      alphanumeric: 452,
+      byte: 310,
+      kanji: 191
+    }
+  },
+  19 => {
+    L: {
+      numeric: 1903,
+      alphanumeric: 1153,
+      byte: 792,
+      kanji: 488
+    },
+    M: {
+      numeric: 1500,
+      alphanumeric: 909,
+      byte: 624,
+      kanji: 384
+    },
+    Q: {
+      numeric: 1063,
+      alphanumeric: 644,
+      byte: 442,
+      kanji: 272
+    },
+    H: {
+      numeric: 813,
+      alphanumeric: 493,
+      byte: 338,
+      kanji: 208
+    }
+  },
+  20 => {
+    L: {
+      numeric: 2061,
+      alphanumeric: 1249,
+      byte: 858,
+      kanji: 528
+    },
+    M: {
+      numeric: 1600,
+      alphanumeric: 970,
+      byte: 666,
+      kanji: 410
+    },
+    Q: {
+      numeric: 1159,
+      alphanumeric: 702,
+      byte: 482,
+      kanji: 297
+    },
+    H: {
+      numeric: 919,
+      alphanumeric: 557,
+      byte: 382,
+      kanji: 235
+    }
+  },
+  21 => {
+    L: {
+      numeric: 2232,
+      alphanumeric: 1352,
+      byte: 929,
+      kanji: 572
+    },
+    M: {
+      numeric: 1708,
+      alphanumeric: 1035,
+      byte: 711,
+      kanji: 438
+    },
+    Q: {
+      numeric: 1224,
+      alphanumeric: 742,
+      byte: 509,
+      kanji: 314
+    },
+    H: {
+      numeric: 969,
+      alphanumeric: 587,
+      byte: 403,
+      kanji: 248
+    }
+  },
+  22 => {
+    L: {
+      numeric: 2409,
+      alphanumeric: 1460,
+      byte: 1003,
+      kanji: 618
+    },
+    M: {
+      numeric: 1872,
+      alphanumeric: 1134,
+      byte: 779,
+      kanji: 480
+    },
+    Q: {
+      numeric: 1358,
+      alphanumeric: 823,
+      byte: 565,
+      kanji: 348
+    },
+    H: {
+      numeric: 1056,
+      alphanumeric: 640,
+      byte: 439,
+      kanji: 270
+    }
+  },
+  23 => {
+    L: {
+      numeric: 2620,
+      alphanumeric: 1588,
+      byte: 1091,
+      kanji: 672
+    },
+    M: {
+      numeric: 2059,
+      alphanumeric: 1248,
+      byte: 857,
+      kanji: 528
+    },
+    Q: {
+      numeric: 1468,
+      alphanumeric: 890,
+      byte: 611,
+      kanji: 376
+    },
+    H: {
+      numeric: 1108,
+      alphanumeric: 672,
+      byte: 461,
+      kanji: 284
+    }
+  },
+  24 => {
+    L: {
+      numeric: 2812,
+      alphanumeric: 1704,
+      byte: 1171,
+      kanji: 721
+    },
+    M: {
+      numeric: 2188,
+      alphanumeric: 1326,
+      byte: 911,
+      kanji: 561
+    },
+    Q: {
+      numeric: 1588,
+      alphanumeric: 963,
+      byte: 661,
+      kanji: 407
+    },
+    H: {
+      numeric: 1228,
+      alphanumeric: 744,
+      byte: 511,
+      kanji: 315
+    }
+  },
+  25 => {
+    L: {
+      numeric: 3057,
+      alphanumeric: 1853,
+      byte: 1273,
+      kanji: 784
+    },
+    M: {
+      numeric: 2395,
+      alphanumeric: 1451,
+      byte: 997,
+      kanji: 614
+    },
+    Q: {
+      numeric: 1718,
+      alphanumeric: 1041,
+      byte: 715,
+      kanji: 440
+    },
+    H: {
+      numeric: 1286,
+      alphanumeric: 779,
+      byte: 535,
+      kanji: 330
+    }
+  },
+  26 => {
+    L: {
+      numeric: 3283,
+      alphanumeric: 1990,
+      byte: 1367,
+      kanji: 842
+    },
+    M: {
+      numeric: 2544,
+      alphanumeric: 1542,
+      byte: 1059,
+      kanji: 652
+    },
+    Q: {
+      numeric: 1804,
+      alphanumeric: 1094,
+      byte: 751,
+      kanji: 462
+    },
+    H: {
+      numeric: 1425,
+      alphanumeric: 864,
+      byte: 593,
+      kanji: 365
+    }
+  },
+  27 => {
+    L: {
+      numeric: 3517,
+      alphanumeric: 2132,
+      byte: 1465,
+      kanji: 902
+    },
+    M: {
+      numeric: 2701,
+      alphanumeric: 1637,
+      byte: 1125,
+      kanji: 692
+    },
+    Q: {
+      numeric: 1933,
+      alphanumeric: 1172,
+      byte: 805,
+      kanji: 496
+    },
+    H: {
+      numeric: 1501,
+      alphanumeric: 910,
+      byte: 625,
+      kanji: 385
+    }
+  },
+  28 => {
+    L: {
+      numeric: 3669,
+      alphanumeric: 2223,
+      byte: 1528,
+      kanji: 940
+    },
+    M: {
+      numeric: 2857,
+      alphanumeric: 1732,
+      byte: 1190,
+      kanji: 732
+    },
+    Q: {
+      numeric: 2085,
+      alphanumeric: 1263,
+      byte: 868,
+      kanji: 534
+    },
+    H: {
+      numeric: 1581,
+      alphanumeric: 958,
+      byte: 658,
+      kanji: 405
+    }
+  },
+  29 => {
+    L: {
+      numeric: 3909,
+      alphanumeric: 2369,
+      byte: 1628,
+      kanji: 1002
+    },
+    M: {
+      numeric: 3035,
+      alphanumeric: 1839,
+      byte: 1264,
+      kanji: 778
+    },
+    Q: {
+      numeric: 2181,
+      alphanumeric: 1322,
+      byte: 908,
+      kanji: 559
+    },
+    H: {
+      numeric: 1677,
+      alphanumeric: 1016,
+      byte: 698,
+      kanji: 430
+    }
+  },
+  30 => {
+    L: {
+      numeric: 4158,
+      alphanumeric: 2520,
+      byte: 1732,
+      kanji: 1066
+    },
+    M: {
+      numeric: 3289,
+      alphanumeric: 1994,
+      byte: 1370,
+      kanji: 843
+    },
+    Q: {
+      numeric: 2358,
+      alphanumeric: 1429,
+      byte: 982,
+      kanji: 604
+    },
+    H: {
+      numeric: 1782,
+      alphanumeric: 1080,
+      byte: 742,
+      kanji: 457
+    }
+  },
+  31 => {
+    L: {
+      numeric: 4417,
+      alphanumeric: 2677,
+      byte: 1840,
+      kanji: 1132
+    },
+    M: {
+      numeric: 3486,
+      alphanumeric: 2113,
+      byte: 1452,
+      kanji: 894
+    },
+    Q: {
+      numeric: 2473,
+      alphanumeric: 1499,
+      byte: 1030,
+      kanji: 634
+    },
+    H: {
+      numeric: 1897,
+      alphanumeric: 1150,
+      byte: 790,
+      kanji: 486
+    }
+  },
+  32 => {
+    L: {
+      numeric: 4686,
+      alphanumeric: 2840,
+      byte: 1952,
+      kanji: 1201
+    },
+    M: {
+      numeric: 3693,
+      alphanumeric: 2238,
+      byte: 1538,
+      kanji: 947
+    },
+    Q: {
+      numeric: 2670,
+      alphanumeric: 1618,
+      byte: 1112,
+      kanji: 684
+    },
+    H: {
+      numeric: 2022,
+      alphanumeric: 1226,
+      byte: 842,
+      kanji: 518
+    }
+  },
+  33 => {
+    L: {
+      numeric: 4965,
+      alphanumeric: 3009,
+      byte: 2068,
+      kanji: 1273
+    },
+    M: {
+      numeric: 3909,
+      alphanumeric: 2369,
+      byte: 1628,
+      kanji: 1002
+    },
+    Q: {
+      numeric: 2805,
+      alphanumeric: 1700,
+      byte: 1168,
+      kanji: 719
+    },
+    H: {
+      numeric: 2157,
+      alphanumeric: 1307,
+      byte: 898,
+      kanji: 553
+    }
+  },
+  34 => {
+    L: {
+      numeric: 5253,
+      alphanumeric: 3183,
+      byte: 2188,
+      kanji: 1347
+    },
+    M: {
+      numeric: 4134,
+      alphanumeric: 2506,
+      byte: 1722,
+      kanji: 1060
+    },
+    Q: {
+      numeric: 2949,
+      alphanumeric: 1787,
+      byte: 1228,
+      kanji: 756
+    },
+    H: {
+      numeric: 2301,
+      alphanumeric: 1394,
+      byte: 958,
+      kanji: 590
+    }
+  },
+  35 => {
+    L: {
+      numeric: 5529,
+      alphanumeric: 3351,
+      byte: 2303,
+      kanji: 1417
+    },
+    M: {
+      numeric: 4343,
+      alphanumeric: 2632,
+      byte: 1809,
+      kanji: 1113
+    },
+    Q: {
+      numeric: 3081,
+      alphanumeric: 1867,
+      byte: 1283,
+      kanji: 790
+    },
+    H: {
+      numeric: 2361,
+      alphanumeric: 1431,
+      byte: 983,
+      kanji: 605
+    }
+  },
+  36 => {
+    L: {
+      numeric: 5836,
+      alphanumeric: 3537,
+      byte: 2431,
+      kanji: 1496
+    },
+    M: {
+      numeric: 4588,
+      alphanumeric: 2780,
+      byte: 1911,
+      kanji: 1176
+    },
+    Q: {
+      numeric: 3244,
+      alphanumeric: 1966,
+      byte: 1351,
+      kanji: 832
+    },
+    H: {
+      numeric: 2524,
+      alphanumeric: 1530,
+      byte: 1051,
+      kanji: 647
+    }
+  },
+  37 => {
+    L: {
+      numeric: 6153,
+      alphanumeric: 3729,
+      byte: 2563,
+      kanji: 1577
+    },
+    M: {
+      numeric: 4775,
+      alphanumeric: 2894,
+      byte: 1989,
+      kanji: 1224
+    },
+    Q: {
+      numeric: 3417,
+      alphanumeric: 2071,
+      byte: 1423,
+      kanji: 876
+    },
+    H: {
+      numeric: 2625,
+      alphanumeric: 1591,
+      byte: 1093,
+      kanji: 673
+    }
+  },
+  38 => {
+    L: {
+      numeric: 6479,
+      alphanumeric: 3927,
+      byte: 2699,
+      kanji: 1661
+    },
+    M: {
+      numeric: 5039,
+      alphanumeric: 3054,
+      byte: 2099,
+      kanji: 1292
+    },
+    Q: {
+      numeric: 3599,
+      alphanumeric: 2181,
+      byte: 1499,
+      kanji: 923
+    },
+    H: {
+      numeric: 2735,
+      alphanumeric: 1658,
+      byte: 1139,
+      kanji: 701
+    }
+  },
+  39 => {
+    L: {
+      numeric: 6743,
+      alphanumeric: 4087,
+      byte: 2809,
+      kanji: 1729
+    },
+    M: {
+      numeric: 5313,
+      alphanumeric: 3220,
+      byte: 2213,
+      kanji: 1362
+    },
+    Q: {
+      numeric: 3791,
+      alphanumeric: 2298,
+      byte: 1579,
+      kanji: 972
+    },
+    H: {
+      numeric: 2927,
+      alphanumeric: 1774,
+      byte: 1219,
+      kanji: 750
+    }
+  },
+  40 => {
+    L: {
+      numeric: 7089,
+      alphanumeric: 4296,
+      byte: 2953,
+      kanji: 1817
+    },
+    M: {
+      numeric: 5596,
+      alphanumeric: 3391,
+      byte: 2331,
+      kanji: 1435
+    },
+    Q: {
+      numeric: 3993,
+      alphanumeric: 2420,
+      byte: 1663,
+      kanji: 1024
+    },
+    H: {
+      numeric: 3057,
+      alphanumeric: 1852,
+      byte: 1273,
+      kanji: 784
+    }
+  }
+}.freeze
+
+CHARACTER_COUNT_INDICATOR = {
+  one_to_nine: {
+    numeric: 10,
+    alphanumeric: 9,
+    byte: 8,
+    kanji: 8
+  },
+  ten_to_twenty_six: {
+    numeric: 12,
+    alphanumeric: 11,
+    byte: 16,
+    kanji: 10
+  },
+  twenty_seven_to_forty: {
+    numeric: 14,
+    alphanumeric: 13,
+    byte: 16,
+    kanji: 12
+  }
+}.freeze
